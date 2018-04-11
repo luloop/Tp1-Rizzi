47d4f4c521dffbacc682a654f54fca97129540a3
Lucila Rizzi <36158159+luloop@users.noreply.github.com>
2018-04-11 17:51:51 -0300

    Modificacion6
    Actualizacion de estilo

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/********************************************************
+* Programa : Tp1_laboratorio1_programacion1             *
+*                                                       *
+* Objetivo : Hace una calculadora con el ingreso de     *
+         2 numeros flotantes                            *
+*                                                       *
+*                                                       *
+*                                                       *
+* Version: 6                                            *
+* Autor: Lucila Rizzi                                   *
+*                                                       *
+********************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h> //biblioteca de getche y getch
@@ -17,19 +30,16 @@ int main()
     while(seguir==0)
     {
         opcion = ingresarMenu();
-
         switch(opcion)
         {
             case 1:
                     primerOperando=pedirEnteroSintope(" \n=============================\n Ingrese el Primero Operando: \t ");
                     flag1=1;
                     break;
-
             case 2:
                     segundooperando=pedirEnteroSintope(" \n=============================\n Ingrese el Segundo Operando: \t ");
                     flag2=1;
                     break;
-
             case 3:
                     primerOperando= validarFlags(flag1, primerOperando ,"\n \t Ingrese el primer operando: \t");
                     flag1=1;
@@ -39,7 +49,6 @@ int main()
                     informarOperadores(primerOperando, segundooperando);
                     printf("La suma es  \t %.2f \t \n \n", operacion);
                     break;
-
             case 4:
                     primerOperando= validarFlags(flag1, primerOperando, "\n \t Ingrese el primer operando: \t");
                     flag1=1;
@@ -49,7 +58,6 @@ int main()
                     informarOperadores(primerOperando, segundooperando);
                     printf("La Resta es  \t %.2f \t \n \n", operacion);
                     break;
-
             case 5:
                     primerOperando= validarFlags(flag1, primerOperando, "\n \t Ingrese el primer operando: \t");
                     flag1=1;
@@ -66,7 +74,6 @@ int main()
                         printf("La Division es  \t %.2f \t \n \n \n", operacion);
                     }
                     break;
-
             case 6:
                    primerOperando= validarFlags(flag1, primerOperando, "\n \t Ingrese el primer operando: \t");
                    flag1=1;
@@ -74,42 +81,32 @@ int main()
                    flag2=1;
                    operacion=multiplicar(primerOperando,segundooperando);
                    informarOperadores(primerOperando, segundooperando);
-
                    printf("La Multiplicacion es  \t %.2f \t \n \n \n", operacion);
                    break;
-
             case 7:
-
                    primerOperando= validarFlags(flag1, primerOperando," \n \t Ingrese el primer operando: \t");
                    flag1=1;
-
                    printf("=======================\nPARA EL OPERANDO: %d \n",(int)primerOperando);
                    printf("=======================\n");
                    operacion=factorial((int)primerOperando);
-
                    printf("\t \t El factorial es:  \t %.0f \n \n", operacion);
                    break;
-
             case 8:
                   primerOperando= validarFlags(flag1, primerOperando, "\n \t Ingrese el primer operando: \t");
                   flag1=1;
                   segundooperando=validarFlags(flag2,segundooperando , "\n \t Ingrese el segundo operando: \t");
                   flag2=1;
-
                   informarOperadores(primerOperando, segundooperando);
                   calcularTodasOperaciones(primerOperando,segundooperando);
                   break;
-
             case 9:
                   seguir = 1;
                   break;
         }
-
         printf("\n");
         system("pause");
         system ("cls");
 }
-
     return 0;
 
 
